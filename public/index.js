dbe49a29f1494d860297cbde4361b6c9de4d5c3e
andremw <mazoni.andre@gmail.com>
2014-11-29 12:03:33 -0200

    Fixing index to call https

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 (function (window, document, undefined) {
 
   // the lib tries to connect to the socket, if it fails it keeps trying
-  var socket = io('http://localhost:5000'),
+  var socket = io('https://localhost:5000'),
       fileInput = document.getElementById('fileInput');
 
   var heartBtns = null,
